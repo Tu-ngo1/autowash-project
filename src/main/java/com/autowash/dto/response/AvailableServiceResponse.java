23957ec458342f2facc8abca83b68f9907c8b894
https://github.com/LittileMonkey <ltn27062005@gmail.com>
2026-06-22 21:47:54 +0700

    22/6 update

--- src/main/java/com/autowash/dto/response/AvailableServiceResponse.java
+++ src/main/java/com/autowash/dto/response/AvailableServiceResponse.java
@@ -1,16 +1,24 @@
 package com.autowash.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvailableServiceResponse {
+
     private Long serviceId;
     private String serviceName;
     private String description;
+
     private Long servicePriceId;
+
     private Integer price;
     private Integer durationMinutes;
+
     private String vehicleSize;
 }
